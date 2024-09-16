51c0f95aa444d75496fff23af7ecb01661fbb12c
Asu-Ghi <digitalasu@gmail.com>
2024-09-16 17:29:52 -0400

    Testing

---
+++ C_Projects/hash_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+	char* key;
+	
+	char* value;
+	struct node* next;
+}
+
+void setNode(struct node*, node, char* key, char* value) {
+
+	node->key = key;
+	node->value = value;
+	node->next = NULL;
+	return;
+}
+
+
+
+
+
