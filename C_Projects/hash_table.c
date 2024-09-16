665745d2acd757a7435c9342e77b5986b735c1f6
Asu-Ghi <digitalasu@gmail.com>
2024-09-16 18:06:45 -0400

    Hashtable changes

--- C_Projects/hash_table.c
+++ C_Projects/hash_table.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+ 
+#define MAX_NAME 256
+#define TABLE_SIZE 10
 
-struct node {
-	char* key;
-	
-	char* value;
-	struct node* next;
+
+typedef struct {
+	char name[MAX_NAME];
+	int age;
+} person;
+
+unsigned int hash(char* name) {
+	int length = strnlen(name, MAX_NAME);
+	unsigned int hash_value = 0;
+	for (int i = 0; i < length; i++) {
+		// Adds ascii value of char to hash_value
+		hash_value += name[i];
+		// adds more random to hash_value and ensures it doesnt exceed table size
+		hash_value = hash_value * name[i] % TABLE_SIZE;
+		
+	}
+	return hash_value;
 }
 
-void setNode(struct node*, node, char* key, char* value) {
-
-	node->key = key;
-	node->value = value;
-	node->next = NULL;
-	return;
+int main() {
+     printf("Jacob => %u\n", hash("Jacob")); 
+     printf("Natalie => %u\n", hash("Natalie")); 
+     printf("Mark => %u\n", hash("Mark")); 
+     printf("John => %u\n", hash("John")); 
+     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
