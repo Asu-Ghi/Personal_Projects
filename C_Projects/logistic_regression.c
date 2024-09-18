4288eaeb8d1159285aab45dc95ba1f799ff6a6ae
Asu Ghimire <92192416+Asu-Ghi@users.noreply.github.com>
2024-09-17 20:39:13 -0400

    LogiReg

--- C_Projects/logistic_regression.c
+++ C_Projects/logistic_regression.c
@@ -2,3 +2,12 @@
 #include <string.h>
 #include <stdbool.h>
 
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("command usage %s, %s", argv[0], "data");
+    }
+    
+
+
+    return 0;
+}
