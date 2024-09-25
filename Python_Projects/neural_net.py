eba26bcfda0304f39128d9c5f7281926fe0b8207
Asu Ghimire <92192416+Asu-Ghi@users.noreply.github.com>
2024-09-25 02:20:57 +0000

    Changes

--- Python_Projects/neural_net.py
+++ Python_Projects/neural_net.py
@@ -1,10 +1,21 @@
 import numpy as np
 
-class Layer:
-    def __init__(self):
-        self.weights = None;
-        self.biases = None;
+# Dense Layer Class
+class Dense_Layer:
+    def __init__(self, n_inputs, n_neurons):
+        # Init weights and biases
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-    def forward(self, prev_layer):
-        pass
+    def forward(self, inputs):
+        # Calculate output from inputs, weights and biases
+        self.output = np.dot(self.weights, inputs) + self.biases;
     
+
+# ReLu Activation Function
+class ReLU:
+    
+    # Forward Pass
+    def forward(self, inputs):
+        # Calculate output values from inputs
+        self.output = np.maximum(0, inputs)
