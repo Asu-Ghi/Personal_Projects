b8a1556b94309854aeda42e18138ebe0a6bf350e
Asu-Ghi <digitalasu@gmail.com>
2024-11-10 20:47:24 -0500

    works

--- C_Projects/Math/binary_to_txt.c
+++ C_Projects/Math/binary_to_txt.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 
-int main() {
-    printf("Workds \n");
+int main(int* argc, char** argv) {
+    if (argc < 3) {
+        printf("Command usage %s %s %s \n", argv[0], "size", "dim");
+    }
+
+    int len = atoi(argv[1]);
+    int dim = atoi(argv[2]);
+
+    // read in binary
+    __uint128_t* data = (__uint128_t*) calloc(len * dim, sizeof(__uint128_t));
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < dim; j++) {
+            scanf("%u")
+        }
+    }
+    
+
+
 }
