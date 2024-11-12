b2b46947be9768aae1b64cde65992025e3971a3c
Asu-Ghi <digitalasu@gmail.com>
2024-11-12 01:43:14 -0500

    forward pass

--- C_Projects/Neural/linalg.h
+++ C_Projects/Neural/linalg.h
@@ -6,7 +6,7 @@ void scalar_mult(double* w, double scalar, int dim);
 
 void print_matrix(double* M, int dim1, int dim2);
 
-double* matrix_mult(double *m1, double *m2, int* dim1, int* dim2) ;
+double* matrix_mult(double* inputs, double* weights, int num_inputs, int num_neurons);
 
 double dot_product(double* w, double* v, int dim);
 
